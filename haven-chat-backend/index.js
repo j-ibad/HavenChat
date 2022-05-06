ca350c019c5d28f710e354b1a82136804d8650f7
Josh Ibad <joshcibad@gmail.com>
2022-05-06 15:08:39 -0700

    Update index.js

--- haven-chat-backend/index.js
+++ haven-chat-backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
+const fs = require('fs');
 
 const UserRouter = require('./controller/UserController.js');
 
